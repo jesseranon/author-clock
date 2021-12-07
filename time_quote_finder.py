ee55258301ac4d5942abb9f6bc41ffb69867dfd0
jranon <jessean.infiltrator@gmail.com>
2021-12-07 13:48:29 -0800

    test

--- time_quote_finder.py
+++ time_quote_finder.py
@@ -11,15 +11,15 @@ def find_time_quote():
 
         csv_dict_reader = DictReader(read_obj)
         column_names = csv_dict_reader.fieldnames
-        print(column_names)
-        # for row in csv_dict_reader:
-        #     # print(row['time-of-quote'], row['author'])
-        #     if row['time-of-text'] == now_time: #change to result of current time conversion function
-        #         filtered_results.append(row)
+        # print(column_names)
+        for row in csv_dict_reader:
+            # print(row['time-of-quote'], row['author'])
+            if row['time-of-text'] == now_time: #change to result of current time conversion function
+                filtered_results.append(row)
 
-        # if len(filtered_results) > 0:
-        #     return filtered_results[random.randint(0,len(filtered_results)-1)]
-        # else:
-        #     return f"No quotes found for {now_time}"
+        if len(filtered_results) > 0:
+            return filtered_results[random.randint(0,len(filtered_results)-1)]
+        else:
+            return f"No quotes found for {now_time}"
 
-find_time_quote()
+# find_time_quote()
